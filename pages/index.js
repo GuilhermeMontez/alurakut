23fd5888adc1bc7e867ce6e5c015d30163b1ac3b
Guilherme Montes <guilmontez@outlook.com>
2021-07-27 22:14:32 -0300

    Sistema de Login, e ultima aula concluida

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
@@ -44,8 +46,9 @@ function ProfileRelationsBox (propriedades) {
   )
 }
 
-export default function Home() {
-  const githubUser = 'GuilhermeMontez';
+export default function Home(props) {
+
+  const githubUser = props.githubUser;
   const [comunidades, setComunidades] = React.useState([]);
   
   const pessoasFavoritas = [
@@ -207,4 +210,32 @@ React.useEffect(function() {
       </MainGrid>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination : '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, 
+  }
 }
